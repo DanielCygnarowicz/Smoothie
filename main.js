24026521f69e6964aa5e31ed35d51edd602d4637
Daniel Cygnarowicz <daniel.cygnarowicz@onet.eu>
2019-06-05 12:31:04 +0200

    version 1.0, add graphics and bacground.

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
-const nav = document.querySelector("nav");
+const list = document.querySelectorAll("nav ul li");
+const sections = document.querySelectorAll("section");
+let sectionsHeight = [];
+sections.forEach((section, index) => { sectionsHeight[index] = section.offsetHeight })
 
-console.log("jest")
+list.forEach((li, i) => {
+  li.addEventListener("click", function () {
+    list.forEach((li) => li.classList.remove("active"));
+    li.classList.add("active");
+    scrollPosition = 0;
+    const index = i;
+    for (let i = 0; i < index; i++) {
+      scrollPosition += sectionsHeight[i];
+    }
+    window.scrollTo(0, scrollPosition);
+  })
+})
+
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+
+
+  if (scroll < (sectionsHeight[0] / 1.5 - list[0].offsetHeight)) {
+    list.forEach((li) => li.classList.remove("active"));
+    list[0].classList.add("active");
+  }
+  else if (scroll < (sectionsHeight[0] + sectionsHeight[1] / 1.5 - list[0].offsetHeight)) {
+    list.forEach((li) => li.classList.remove("active"));
+    list[1].classList.add("active");
+  }
+  else if (scroll < (sectionsHeight[0] + sectionsHeight[1] + sectionsHeight[2] / 1.5 - list[0].offsetHeight)) {
+    list.forEach((li) => li.classList.remove("active"));
+    list[2].classList.add("active");
+  }
+  else if (scroll < (sectionsHeight[0] + sectionsHeight[1] + sectionsHeight[2] + sectionsHeight[3] / 1.5 - list[0].offsetHeight)) {
+    list.forEach((li) => li.classList.remove("active"));
+    list[3].classList.add("active");
+  }
+  else if (scroll < (sectionsHeight[0] + sectionsHeight[1] + sectionsHeight[2] + sectionsHeight[3] + sectionsHeight[4] / 1.5 - list[0].offsetHeight)) {
+    list.forEach((li) => li.classList.remove("active"));
+    list[4].classList.add("active");
+  }
+  else if (scroll < (sectionsHeight[0] + sectionsHeight[1] + sectionsHeight[2] + sectionsHeight[3] + sectionsHeight[4] + sectionsHeight[5] / 1.5 - list[0].offsetHeight)) {
+    list.forEach((li) => li.classList.remove("active"));
+    list[5].classList.add("active");
+  }
+  else {
+    list.forEach((li) => li.classList.remove("active"));
+    list[6].classList.add("active");
+  }
+
+})
